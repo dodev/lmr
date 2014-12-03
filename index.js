c6a73155ef06a26eca2ca3310806691ca4af9615
Petar Dodev <petar.dodev@gmail.com>
2014-12-03 17:30:51 +0300

    Added plan for the module development

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+'use strict';
+
+var PWD = process.env.PWD;
+var root = PWD + '/';
+
+// 1.look for config
+// 2.determine root
+// 3.build aliases
+
 function lmr(modulePath) {
-    return require(modulePath);
+    // resolve module path
+    // look for module in aliases
+    //    if present return the required moduel
+    //    if not present require from the fs
+    //    if not present in aliases and fs throw error
+    return require(root + modulePath);
 }
 
 module.exports = lmr;

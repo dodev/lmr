796fa6a68bfc276f069958e79a5c875b9ac17654
Petar Dodev <petar.dodev@gmail.com>
2014-12-04 16:00:38 +0300

    Added the first test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 require('should');
+var lmr = require('../index.js');
 
 describe('lmr', function () {
     it('should do something', function () {
-
+        var baz = lmr('test/baz');
+        baz.value.should.equal('baz');
     });
 });
